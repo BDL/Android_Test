9404fc94dd463e845d8b1c0c722b7528d83a6db0
BDL <lester69@gmail.com>
2017-01-24 11:10:13 -0500

    This is a commit message....

---
+++ app/src/main/java/com/example/lester69/chaptertwo/ch2.java
@@ -0,0 +1,15 @@
+package com.example.lester69.chaptertwo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ch2 extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ch2);
+    }
+}
